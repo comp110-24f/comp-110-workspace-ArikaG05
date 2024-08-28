e91d8d46a2c01fbf58241a52b7ba3000f3bdbd38
Arika Gardner <arikag@unc.edu>
2024-08-28 08:46:11 -0400

    Aug 28, 2024 8:46 AM

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,16 @@
+__author__ = "730646316"
+
+
+def mimic(message: str) -> str:
+    """Return the message back to me"""
+    return message
+
+
+def main() -> None:
+    """Print calling mimic message"""
+    print(mimic(message=input("What is your message?")))
+    return None
+
+
+if__name__ = "__main__"
+main()
